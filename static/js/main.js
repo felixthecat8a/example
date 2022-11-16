bfe466dabc90ed62aa56f6742b94507065178934
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2022-11-15 22:07:28 -0600

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,39 +2,27 @@ const url = `https://api.thecatapi.com/v1/breeds`;
 const api_key = 'live_8e9vqpLpntUSCiumthQu2zHnvYwMOIMF1JLdWpcUKeqztLa53mfjoZcz3GrymaBh';
 let storedBreeds = []
 
- fetch(url,{headers: {
-      'x-api-key': api_key
-    }})
- .then((response) => {
-   return response.json();
- })
-.then((data) => {
-   
-   //filter to only include those with an `image` object
-   data = data.filter(img=> img.image?.url!=null)
-   
-  storedBreeds = data;
-   
-   for (let i = 0; i < storedBreeds.length; i++) {
-    const breed = storedBreeds[i];
-    let option = document.createElement('option');
-     
-     //skip any breeds that don't have an image
-     if(!breed.image)continue
-     
-    //use the current array index
-    option.value = i;
-    option.innerHTML = `${breed.name}`;
-document.getElementById('breed_selector').appendChild(option);
-    
-    }
-   //show the first breed by default
-   showBreedImage(0)
-})
-.catch(function(error) {
-   console.log(error);
-});
+async function start() {
+    try {
+        const response = await (await fetch(url,{headers: {'x-api-key':api_key}}))
+        const data = await response.json()
+        createBreedList(data)
+        showBreedImage(0)
+    } catch (error) {console.log("There was a problem fetching the breed list.")}
+}
+start()
 
+function createBreedList (data) {
+    storedBreeds = data.filter(img=> img.image?.url!=null);
+    for (let i = 0; i < storedBreeds.length; i++) {
+        const breed = storedBreeds[i];
+        let option = document.createElement('option');
+        if(!breed.image)continue; //skip any breeds that don't have an image
+        option.value = i; //use the current array index
+        option.innerHTML = `${breed.name}`;
+        document.getElementById('breed_selector').appendChild(option);
+    }
+}
 function showBreedImage(index)
 { 
   document.getElementById("breed_image").src= storedBreeds[index].image.url;
