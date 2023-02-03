e73e4a8f75c05af6d3147bdc6a88aaf218a720cc
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-02-02 23:22:59 -0600

    Update game.js

--- static/js/game.js
+++ static/js/game.js
@@ -25,6 +25,7 @@ function playGame() {
     const guess = document.getElementById("guess")
     const result = document.getElementById("result")
     const attempt = document.getElementById("attempt")
+    
     let randomNumber = (Math.floor(Math.random() * 100) + 1)
     console.log(randomNumber)
     let attempts = 0
@@ -39,36 +40,28 @@ function playGame() {
     check.addEventListener("click", guessNumber)
     function guessNumber() {
         attempts++
-        if (guess.value < randomNumber) {
+        if (guess.value < randomNumber && attempts != 10) {
             result.innerHTML = "<div style='color:cornflowerblue'>Your number is too low. Try again.</div>"
-            attempt.innerHTML = `<h3>Attempt number ${attempts}.</h3>`
-        } else if (guess.value > randomNumber) {
+            attempt.innerHTML = `<h3>Attempts: ${attempts}</h3>`
+        } else if (guess.value > randomNumber && attempts != 10) {
             result.innerHTML = "<div style='color:palevioletred'>Your number is too high. Try again.</div>"
-            attempt.innerHTML = `<h3>Attempt number ${attempts}.</h3>`
+            attempt.innerHTML = `<h3>Attempts: ${attempts}</h3>`
+        } else if (guess.value != randomNumber && attempts >= 10) {
+			document.getElementById("final").innerHTML = `<h3>The number was ${randomNumber}.</h3>`
+			result.innerHTML = "<div style='color:violet'>Sorry, you've reached the attempt limit.</div>"
+            attempt.innerHTML = `<h3>Attempts: ${attempts}</h3>`
         } else {
             document.getElementById("final").innerHTML = `<h3>The number is ${guess.value}!</h3>`
             result.innerHTML = "<div style='color:gold'>You guessed it right!</div>"
-            attempt.innerHTML = `<h3>It only took ${attempts} ${attemptsMessage()}.</h3>`
-            game.innerText = "Play Again?"
-        }
-        if (attempts > 10) {
-            reachedLimit()
+            attempt.innerHTML = `<h3>It only took ${attempts} ${tries()}.</h3>`
         }
     }
     
-    function attemptsMessage() {
+    function tries() {
         if (attempts === 1) {
             return "try"
         } else {
             return "tries"
         }
     }
-    
-    function reachedLimit() {
-        document.getElementById("final").innerHTML = ""
-        result.innerHTML = "<div style='color:mediumorchid'>Sorry, you've reached the limit of attempts.</div>"
-        attempt.innerHTML = `<h3>The number was ${randomNumber}.</h3>`
-        game.innerText = "Play Again?"
-    }
-    
 }
