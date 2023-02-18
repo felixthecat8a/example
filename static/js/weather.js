bb51fc97076f7b27bb7a9687396c4636256d17a7
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-02-18 02:06:49 -0600

    Add files via upload

---
+++ static/js/weather.js
@@ -0,0 +1,27 @@
+//https://api.weather.gov/points/26.3085,-98.1031
+const office = 'BRO';
+const grid_X = '53';
+const grid_Y = '23';
+const endpoint = `https://api.weather.gov/gridpoints/${office}/${grid_X},${grid_Y}/forecast`;
+
+async function displayWeather() {
+    try {
+        const response = await fetch(`${endpoint}`);
+        const data = await response.json();
+
+        const index = 0;
+        const temperature = data.properties.periods[index].temperature;
+        const windSpeed = data.properties.periods[index].windSpeed;
+        const windDirection = data.properties.periods[index].windDirection;
+        const shortForecast = data.properties.periods[index].shortForecast;
+        const detailedForecast = data.properties.periods[index].detailedForecast;
+    
+        const weatherApp = document.getElementById("weatherApp")
+        weatherApp.innerHTML = (`
+        <h2 style='color:darkseagreen;' title='${detailedForecast}'>
+            Alton, Tx: ${temperature}&degF, ${shortForecast}<br>Wind: ${windSpeed} ${windDirection}
+        </h2>
+        `)
+    } catch (error) {console.log("Failed to get weather data")}
+}
+displayWeather()
