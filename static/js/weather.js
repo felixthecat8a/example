69ec744ba45495ff5ae3ac7331d34cd5960afbb9
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-02-28 21:04:25 -0600

    Update weather.js

--- static/js/weather.js
+++ static/js/weather.js
@@ -10,25 +10,43 @@ async function displayWeather() {
         const data = await response.json();
 
         const index = 0;
-        const temperature = data.properties.periods[index].temperature;
-        const windSpeed = data.properties.periods[index].windSpeed;
-        const windDirection = data.properties.periods[index].windDirection;
-        const shortForecast = data.properties.periods[index].shortForecast;
-    
-        const weatherLocation = "Edinburg, TX"
-        const temperatureDisplay = `Temperature: ${temperature}&degF`;
-        const windDisplay = `Wind: ${windSpeed} ${windDirection}`;
+        const forecastData = getForecastData(data,index)
 
         const weatherApp = document.getElementById("weatherApp")
-        weatherApp.innerHTML = (`
-        <div id='weatherTitle'>
-            <h3>${weatherLocation}:<br> ${shortForecast}</h3>
-        </div>
-        <div id='weatherContent'>
-            <h4>${temperatureDisplay}</h4>
-            <h4>${windDisplay}</h4>
-        </div>
-        `)
+        weatherApp.innerHTML = (`${forecastData}`)
+
+        const nwsTitle = document.createElement('h3')
+        nwsTitle.setAttribute('id','nwsTitle')
+        const nwsText = document.createTextNode('National Weather Service API')
+        nwsTitle.appendChild(nwsText)
+        weatherApp.insertAdjacentElement('beforebegin',nwsTitle)
+        
     } catch (error) {console.log("Failed to get weather data")}
 }
 displayWeather()
+
+function getForecastData(data,index) {
+    const temperature = data.properties.periods[index].temperature;
+    const windSpeed = data.properties.periods[index].windSpeed;
+    const windDirection = data.properties.periods[index].windDirection;
+    const shortForecast = data.properties.periods[index].shortForecast;
+    const rain = data.properties.periods[index].probabilityOfPrecipitation.value;
+    const humidity = data.properties.periods[index].relativeHumidity.value;
+
+    const location = "Edinburg, TX"
+    const temperatureDisplay = `Temperature: ${temperature}&degF`;
+    const windDisplay = `Wind: ${windSpeed} ${windDirection}`;
+    const humidityDisplay = `Humidity: ${humidity}%`
+    const rainDisplay = `Chance of Rain: ${rain}%`
+
+    const forecastData = 
+    `
+    <div id='weatherTitle'>
+        <h3>${location}:<br>JEHS<br>${shortForecast}</h3>
+    </div>
+    <div id='weatherContent'>
+        <h4>${temperatureDisplay}<br>${windDisplay}<br>${humidityDisplay}<br>${rainDisplay}</h4>
+    </div>
+    `
+    return forecastData
+}
