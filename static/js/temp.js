3d45384d58ea93168fc55484f5172f566b702a03
Felix Ochoa <115331149+felixthecat8a@users.noreply.github.com>
2023-04-01 14:27:09 -0500

    Update temp.js

--- static/js/temp.js
+++ static/js/temp.js
@@ -1,8 +1,8 @@
-var celciusVal = (document.getElementById("celciusVal"))
+var celsiusVal = (document.getElementById("celsiusVal"))
 var fahrenheitVal = (document.getElementById('fahrenheitVal'))
 const convert = (document.getElementById("convert"))
 
-celciusVal.addEventListener("keypress", function(event) {
+celsiusVal.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
       event.preventDefault()
       convert.click()
@@ -18,20 +18,20 @@ fahrenheitVal.addEventListener("keypress", function(event) {
 
 convert.addEventListener("click", toFahrenheit)
 function toFahrenheit() {
-    if (celciusVal.value) {
-        var fahrenheitOutput = convertCtoF(celciusVal)
+    if (celsiusVal.value) {
+        var fahrenheitOutput = convertCtoF(celsiusVal)
         fahrenheitVal.value = fahrenheitOutput.toFixed(2)
-        let kelvin = convertCtoK(celciusVal)
+        let kelvin = convertCtoK(celsiusVal)
         console.log('Temperature in Kelvin: ',kelvin.toFixed(2),'K')
     } else {fahrenheitVal.value = ''}
 }
 
-fahrenheitVal.addEventListener("input", toCelcius)
-function toCelcius() {
+fahrenheitVal.addEventListener("input", toCelsius)
+function toCelsius() {
     if (fahrenheitVal.value) {
-        var celciusOutput = convertFtoC(fahrenheitVal)
-        celciusVal.value = celciusOutput.toFixed(2)
-    } else {celciusVal.value = ''}
+        var celsiusOutput = convertFtoC(fahrenheitVal)
+        celsiusVal.value = celsiusOutput.toFixed(2)
+    } else {celsiusVal.value = ''}
 }
 
 function convertCtoF(x) {
