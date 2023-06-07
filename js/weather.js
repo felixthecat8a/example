e3121c3ac986b2beda6c9385a858d3c0bd4853d8
felixthecat8a <115331149+felixthecat8a@users.noreply.github.com>
2023-06-06 22:58:22 -0500

    adjustment to temp_app page

--- js/weather.js
+++ js/weather.js
@@ -160,7 +160,7 @@ async function displayCat() {
         const response = await (await fetch(cat))
         const data = await response.json()
         const catImage = data[0].url;
-        const weatherData = `<img src="${catImage}" alt="cat" height="auto" width="350">`;
+        const weatherData = `<img src="${catImage}" alt="cat" height="250" width="auto">`;
         const weatherDisplay = document.getElementById("weatherApp");
         weatherDisplay.innerHTML = (`${weatherData}`);
     } catch (error) {console.log("There was a problem fetching a cat.")}
