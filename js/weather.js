801b1a137cf9abf9bfd85f3caa0a87c6b9eade5f
felixthecat8a <115331149+felixthecat8a@users.noreply.github.com>
2023-09-16 23:42:48 -0500

    weather app overhaul

--- js/weather.js
+++ js/weather.js
@@ -1,71 +1,50 @@
 class WeatherDisplay {
     constructor() {
-        this.weatherDisplay = document.getElementById("weatherDisplay");
+        this.weatherDisplayDIV = document.getElementById("weatherDisplay")
     }
     async create(latitude, longitude) {
         const pointsURL = (`https://api.weather.gov/points/${latitude},${longitude}`);
         try {
             const data = await (await fetch(pointsURL)).json();
-            const endpoint = data.properties.forecastHourly
-            const location = this.getLocation(data)
-            await this.displayWeather(endpoint, location)
             await this.displayForecast(data.properties.forecast)
+            await this.displayCurrentWeather(data)
         }
         catch (error) {
-            weatherStatusDIV.innerText = "Failed to get location data";
-            console.log("Failed to get location data");
+            weatherStatusDIV.innerText = "Failed to get weather API data";
+            console.log("Failed to get weather API data");
         }
     }
-    getLocation(data) {
-        const locationData = data.properties.relativeLocation.properties
-        return (`${locationData.city}, ${locationData.state}`);
-    }
-    async displayWeather(endpoint, location) {
-        console.log(`${location}: ${endpoint}`)
+    async displayForecast(endpoint) {
         try {
-            const response = await fetch(endpoint);
-            const data = await response.json();
-            const weatherHTML = this.getWeatherData(data, location);
-            this.weatherDisplay.innerHTML = weatherHTML;
+            const response = await fetch(endpoint)
+            const data = await response.json()
+            let index = 0;
+            const forecastData = data.properties.periods[index]
+            const forecastHTML = (`<div id='weatherContainer'>
+                <div id="weatherDiv">
+                    <section id='weatherTitle'>
+                        <div style="font-size:large;">${forecastData.name}:</div>
+                        <div style="font-size:medium;">${forecastData.temperature}&degF</div>
+                        <img src="${forecastData.icon}" alt="icon" title="${forecastData.detailedForecast}">
+                    </section>
+                    <section id='weatherContent'>
+                        <div id="currentWeather"></div>
+                    </section>
+                </div>
+                <div id="futureForecastDiv"></div>
+            </div>`);
+            this.weatherDisplayDIV.innerHTML = forecastHTML
+            await this.displayFutureForecast(endpoint)
         }
         catch (error) {
-            weatherStatusDIV.innerText = "Failed to get weather data";
-            console.log("Failed to get weather data");
+            weatherStatusDIV.innerText = "Failed to get forecast data";
+            console.log("Failed to get forecast data");
         }
     }
-    getWeatherData(data, location) {
-        let index = 0;
-        const weatherData = data.properties.periods[index]
-        const temperature = weatherData.temperature;
-        const windSpeed = weatherData.windSpeed;
-        const windDirection = weatherData.windDirection;
-        const wind = `${windSpeed} ${windDirection}`;
-        const shortForecast = weatherData.shortForecast;
-        const humidity = weatherData.relativeHumidity.value;
-        const chanceOfRain = weatherData.probabilityOfPrecipitation.value;
-        const rain = chanceOfRain == null ? "0" : chanceOfRain;
-        const weatherHTML = (`<div id='weatherContainer'>
-            <div id="weatherDiv">
-                <section id='weatherTitle'>
-                    <div id='forecastContent'></div>
-                </section>
-                <section id='weatherContent'>
-                    <div style="font-size:large;">${location}</div>
-                    <div style="font-size:xx-large;">${temperature}&degF</div>
-                    <div style="font-size:x-large;">Wind: ${wind}</div>
-                    <div style="font-size:medium;">Rain: ${rain}% Humidity: ${humidity}%</div>
-                    <div style="font-size:small;">${shortForecast}</div>
-                </section>
-            </div>
-            <div id="forecastDiv"></div>
-        </div>`);
-        return weatherHTML;
-    }
-    async displayForecast(forecastEndpoint) {
-        const forecastDiv = document.getElementById("forecastDiv");
-        const response = await fetch(forecastEndpoint);
+    async displayFutureForecast(endpoint) {
+        const forecastDiv = document.getElementById("futureForecastDiv");
+        const response = await fetch(endpoint);
         const data = await response.json();
-        this.currentForecast(data);
         for (let index = 1; index < 13; index++) {
             const forecastData = data.properties.periods;
             const name = forecastData[index].name;
@@ -75,12 +54,14 @@ class WeatherDisplay {
             const chanceOfRain = forecastData[index].probabilityOfPrecipitation.value;
             const rain = chanceOfRain == null ? "0" : chanceOfRain;
             const icon = forecastData[index].icon;
-            const forecastHTML = (`<div title="${name}: ${detailedForecast}"
+            const forecastHTML = (`
+            <div title="${name}: ${detailedForecast}">
                 <span style="color:lightgreen">${name.substring(0, 3)}:</span> ${rain}%<br>
                 <span style="color:lightcoral">${temperatureHigh}&degF</span><br>
                 <span style="color:lightblue">${temperatureLow}&degF</span><br>
                 <img src="${icon}" alt="icon" height="auto" width="75%" >
-            </div>`);
+            </div>
+            `);
             const dayTime = forecastData[index].isDaytime;
             if (!dayTime) {
                 continue;
@@ -93,24 +74,40 @@ class WeatherDisplay {
             }
         }
     }
-    currentForecast(data){
-        let index = 0;
-        const forecastData = data.properties.periods[index]
-        const forecastContentData = (`
-            <div style="font-size:large;">${forecastData.name}:</div>
-            <div style="font-size:medium;">${forecastData.temperature}&degF</div>
-            <img src="${forecastData.icon}" alt="icon" title="${forecastData.detailedForecast}">
-        `);
-        const forecastContent = document.getElementById('forecastContent');
-        forecastContent.innerHTML = forecastContentData;
-
-    }
-    async createWithEndpoint(endpoint, location) {
-        const parsedUrl = new URL(endpoint);
-        const path = parsedUrl.pathname.split("/").slice(2, 4).join("/");
-        const locationFromEndpoint = location == null ? path : location;
-        await this.displayWeather(`${endpoint}/hourly`, locationFromEndpoint);
-        await this.displayForecast(endpoint);
+    async displayCurrentWeather(data) {
+        const endpoint = data.properties.forecastHourly
+        const locationData = data.properties.relativeLocation.properties
+        const location = (`${locationData.city}, ${locationData.state}`)
+        console.log(`${location}: ${endpoint}`)
+        try {
+            const response = await fetch(endpoint)
+            const data = await response.json()
+            let index = 0;
+            const weatherData = data.properties.periods[index]
+            const temperature = weatherData.temperature;
+            const windSpeed = weatherData.windSpeed;
+            const windDirection = weatherData.windDirection;
+            const wind = `${windSpeed} ${windDirection}`;
+            const shortForecast = weatherData.shortForecast;
+            const humidity = weatherData.relativeHumidity.value;
+            const chanceOfRain = weatherData.probabilityOfPrecipitation.value;
+            const rain = chanceOfRain == null ? "0" : chanceOfRain;
+            const currentWeatherHTML = (`
+            <div style="font-size:large;">${location}</div>
+            <div style="font-size:xx-large;">${temperature}&degF</div>
+            <div style="font-size:x-large;">Wind: ${wind}</div>
+            <div style="font-size:medium;">Humidity: ${humidity}%</div>
+            <div style="font-size:medium;">Chance of Rain: ${rain}%</div>
+            <br>
+            <div style="font-size:small;">${shortForecast}</div>
+            `);
+            const currentWeatherDIV = document.getElementById('currentWeather')
+            currentWeatherDIV.innerHTML = currentWeatherHTML
+        }
+        catch (error) {
+            weatherStatusDIV.innerText = "Failed to get current weather data"
+            console.log("Failed to get current weather data")
+        }
     }
 }
 const weatherDisplay = new WeatherDisplay();
@@ -121,7 +118,7 @@ const jehsPoints = ({
     longitude: '-98.103',
     endpoint: "https://api.weather.gov/gridpoints/BRO/54,24/forecast",
 });
-weatherDisplay.createWithEndpoint(jehsPoints.endpoint,jehsPoints.location);
+weatherDisplay.create(jehsPoints.latitude, jehsPoints.longitude);
 /********************************************************************************************/
 const nwsLink = document.getElementById('nwsLink');
 const selectLocationSELECT = document.getElementById('selectLocation');
@@ -178,7 +175,7 @@ async function displayCat() {
         const data = await response.json();
         const catImage = data[0].url;
         const catDisplay = document.getElementById("weatherDisplay");
-        catDisplay.innerHTML = (`<img src="${catImage}" alt="cat" height="250" width="auto">`);
+        catDisplay.innerHTML = (`<img src="${catImage}" alt="cat" height="300" width="auto">`);
     }
     catch (error) {
         weatherStatusDIV.innerText = "There was a problem fetching a cat.";
