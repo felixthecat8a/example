968805b776862a05c9e0f2f4ff3c4fb5001dc8e7
felixthecat8a <115331149+felixthecat8a@users.noreply.github.com>
2023-11-05 14:30:21 -0600

    added graphs

---
+++ js/graphWeather.js
@@ -0,0 +1,165 @@
+async function getGeoLocation() {
+    return new Promise((resolve, reject) => {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => resolve(position.coords),
+                (error) => reject('Unable to retrieve location')
+            );
+        } else {
+            reject('Geolocation not supported by your browser.');
+        }
+    });
+}
+async function getGeoEndpoint() {
+    try {
+        const coords = await getGeoLocation();
+        const NWS_URL = `https://api.weather.gov/points/${coords.latitude},${coords.longitude}`
+        const response = await fetch(NWS_URL);
+        const data = await response.json();
+        const endpoint = data.properties.forecast;
+        return endpoint;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+async function getForecastData(useGeolocation) {
+    try {
+        let endpoint;
+        if (useGeolocation) {
+            endpoint = await getGeoEndpoint();
+        } else {
+            endpoint = 'https://api.weather.gov/gridpoints/BRO/46,23/forecast/';
+        }
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        const daytimeData = data.properties.periods.filter((period) => period.isDaytime);
+        const nighttimeData = data.properties.periods.filter((period) => !period.isDaytime);
+        const dayLabels = daytimeData.map((period) => period.name);
+        const dayTemperature = daytimeData.map((period) => period.temperature);
+        const nightTemperature = nighttimeData.map((period) => period.temperature);
+        const rainChance = daytimeData.map((period) => period.probabilityOfPrecipitation.value);
+        return { dayLabels , dayTemperature, nightTemperature, rainChance };
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+document.addEventListener('DOMContentLoaded', async () => {
+    const { temperatureChart, rainChanceChart } = await createForecastChart()
+    console.log(temperatureChart)
+    const selectForecastGraph = document.getElementById('selectForecastGraph')
+    selectForecastGraph.addEventListener('change', async (event) => {
+        const graphSelection = event.target.value;
+        if (graphSelection == 'geolocation') {
+            await updateForecastChart(temperatureChart, rainChanceChart , true);
+        }
+        else {
+            await updateForecastChart(temperatureChart, rainChanceChart , false);
+        }
+    });
+});
+async function createForecastChart() {
+    const forecastData = await getForecastData(false);
+    const ctx = document.getElementById("temperatureChart");
+    const temperatureChart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: forecastData.dayLabels,
+            datasets: [
+                {
+                    label: "Highs",
+                    data: forecastData.dayTemperature,
+                    borderColor: "rgba(192, 75, 75, 1)",
+                    fill: false,
+                },
+                {
+                    label: "Lows",
+                    data: forecastData.nightTemperature,
+                    borderColor: "rgba(75, 192, 192, 1)",
+                    fill: false,
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Forecast Temperatures'
+                }
+            },
+            scales: {
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Temperature',
+                    },
+                },
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Day',
+                    }
+                }
+            },
+        }
+    });
+    temperatureChart.resize(500,400)
+    const ctx2 = document.getElementById("rainChanceChart");
+    const rainChanceChart = new Chart(ctx2, {
+        type: "bar",
+        data: {
+            labels: forecastData.dayLabels,
+            datasets: [
+                {
+                    label: "Chance of Rain",
+                    data: forecastData.rainChance,
+                    backgroundColor: "rgba(54, 162, 235, 0.6)",
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    max: 100,
+                    title: {
+                        display: true,
+                        text: '% Chance of Rain',
+                        color: 'gray',
+                        font: {
+                            size: 14,
+                        },
+                    },
+                },
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Day',
+                        color: 'gray',
+                        font: {
+                            size: 14,
+                        },
+                    }
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Forecast Chance of Rain'
+                }
+            }
+        },
+    });
+    rainChanceChart.resize(500,400)
+    return { temperatureChart, rainChanceChart }
+}
+async function updateForecastChart(temperatureChart, rainChanceChart, useGeolocation) {
+    const forecastData = await getForecastData(useGeolocation);
+    temperatureChart.data.datasets[0].data = forecastData.dayTemperature
+    temperatureChart.data.datasets[1].data = forecastData.nightTemperature
+    temperatureChart.update()
+    rainChanceChart.data.datasets[0].data = forecastData.rainChance
+    rainChanceChart.update()
+}
