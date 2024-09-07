429d062dd961d6f61b4d80ce5e6079aa6003d936
felixthecat8a <115331149+felixthecat8a@users.noreply.github.com>
2024-09-07 10:10:45 -0500

    updated page

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ const gameDIV = document.getElementById("numberGuessingGame");
 const gameButton = '<button type="button" onclick="playGame(true)">Click to Play</button>';
 document.addEventListener('DOMContentLoaded', () => { gameDIV.innerHTML = gameButton; });
 function playGame(debugGame) {
-    window.animatelo.jackInTheBox('#numberGuessingGameContainer')
+    window.animatelo.jackInTheBox('#numberGuessingGameContainer');
     const MAX_ATTEMPTS = 7;
     gameDIV.innerHTML = (`
     <h3 id="heading">Guess the number from 1 to 100<br>in ${MAX_ATTEMPTS} tries or less.</h3>
@@ -22,60 +22,54 @@ function playGame(debugGame) {
     const playAgainButton = document.getElementById("playAgain");
     const close = document.getElementById("close");
     const answer = (Math.floor(Math.random() * 100) + 1);
-    if (debugGame) {
-        console.log(answer);
-    }
+    if (debugGame) { console.log(answer) };
     let attempts = 0;
     guessInput.addEventListener("keypress", function (event) {
         if (event.key === "Enter") {
             event.preventDefault();
             checkButton.click();
-        }
+        };
     });
     checkButton.addEventListener("click", () => {
         if (guessInput.value) {
             attempts++;
             attemptMeter.value = attempts;
             checkNumber(Number(guessInput.value));
-        }
+        };
     });
     function checkNumber(guessNumber) {
         if (attempts < MAX_ATTEMPTS || guessNumber == answer) {
             if (guessNumber < answer) {
                 window.animatelo.shake('#heading');
                 updateHeading('Your number is too low.\nTry again.', 'cornflowerblue');
-            }
-            else if (guessNumber > answer) {
+            } else if (guessNumber > answer) {
                 window.animatelo.shake('#heading');
                 updateHeading('Your number is too high.\nTry again.', 'palevioletred');
-            }
-            else {
+            } else {
                 window.animatelo.rubberBand('#heading');
                 updateHeading(`You guessed it right!\nThe number is ${answer}!`, 'gold', true);
                 disableCheckButton();
-            }
-        }
-        else {
+            };
+        } else {
             window.animatelo.flash('#heading');
             updateHeading(`You've reached the limit.\nThe number was ${answer}.`, 'orchid', false);
             disableCheckButton();
-        }
-    }
+        };
+    };
     function updateHeading(text, textColor, success) {
         heading.innerText = text;
         heading.style.color = textColor;
         if (!success) {
             attemptLabel.innerText = `Attempts Left: ${MAX_ATTEMPTS - attempts}`;
-        }
-        else {
+        } else {
             const tries = (attempts === 1) ? 'try' : 'tries';
             attemptLabel.innerText = `It only took ${attempts} ${tries}.`;
-        }
-    }
+        };
+    };
     function disableCheckButton() {
         checkButton.disabled = true;
         checkButton.style.opacity = '0.5';
-    }
+    };
     playAgainButton.onclick = function () {
         attemptMeter.value = 0;
         attempts = 0;
@@ -87,4 +81,4 @@ function playGame(debugGame) {
         attempts = 0;
         gameDIV.innerHTML = gameButton;
     });
-}
+};
