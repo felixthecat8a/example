bd0c102d64bf7f00da7f256beb22a2b51233923b
Felix Ochoa <fo59paz@yahoo.com>
2025-09-21 21:55:57 -0500

    formatting updates

--- src/ts/temperature.ts
+++ src/ts/temperature.ts
@@ -3,35 +3,35 @@ const fahrenheitVal = document.getElementById('fahrenheitVal') as HTMLInputEleme
 const convert = document.getElementById('convert') as HTMLButtonElement
 
 celsiusVal.addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault()
-        convert.click()
-    }
+  if (event.key === 'Enter') {
+    event.preventDefault()
+    convert.click()
+  }
 })
 
 fahrenheitVal.addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-        event.preventDefault()
-        convert.click()
-    }
+  if (event.key === 'Enter') {
+    event.preventDefault()
+    convert.click()
+  }
 })
 
 convert.addEventListener('click', toFahrenheit)
 function toFahrenheit() {
-    if (celsiusVal.value) {
-        var fahrenheitOutput = parseFloat(celsiusVal.value) * 1.8 + 32
-        fahrenheitVal.value = fahrenheitOutput.toFixed(2)
-    } else {
-        fahrenheitVal.value = ''
-    }
+  if (celsiusVal.value) {
+    var fahrenheitOutput = parseFloat(celsiusVal.value) * 1.8 + 32
+    fahrenheitVal.value = fahrenheitOutput.toFixed(2)
+  } else {
+    fahrenheitVal.value = ''
+  }
 }
 
 fahrenheitVal.addEventListener('input', toCelsius)
 function toCelsius() {
-    if (fahrenheitVal.value) {
-        var celsiusOutput = (parseFloat(fahrenheitVal.value) - 32) / 1.8
-        celsiusVal.value = celsiusOutput.toFixed(2)
-    } else {
-        celsiusVal.value = ''
-    }
+  if (fahrenheitVal.value) {
+    var celsiusOutput = (parseFloat(fahrenheitVal.value) - 32) / 1.8
+    celsiusVal.value = celsiusOutput.toFixed(2)
+  } else {
+    celsiusVal.value = ''
+  }
 }
